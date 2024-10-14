bff321fb40c8db2c9e62558b428f366d7460b590
Isadora-L-Silva <141789840+Isadora-L-Silva@users.noreply.github.com>
2024-10-13 23:56:19 -0300

    init

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function(){
+    $(".navbar .nav-link").on('click', function(event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 700, function(){
+                window.location.hash = hash;
+            });
+        } 
+    });
+});
+
+$('#nav-toggle').click(function(){
+    $(this).toggleClass('is-active')
+    $('ul.nav').toggleClass('show');
+});
